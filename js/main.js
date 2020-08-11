15a5186b6da6ff8fea8896e11c4eefae8cf64719
BenjaminSao <ben.saobuppha@mail.utoronto.ca>
2020-08-10 23:39:05 -0400

    improved aesthetics and added bulma

--- js/main.js
+++ js/main.js
@@ -56,20 +56,110 @@ Vue.component('itemdialogue', {
   },
   //HTML code (v-model assigns data to data variables)
   template: `
-    <div>
-      <form>
-        <div class="form-group">
-          <label>Menu Item Name</label><br>
-          <input v-model="itemName"><br>
-          <label>Description</label><br>
-          <textarea v-model="itemDescription"></textarea><br>
-          <label>Price<br>$</label>
-          <input v-model="itemPrice"><br>
-          <button type="button" class="btn btn-primary" @click="onSubmit">Add to Menu</button>
-          <button @click="collaspeDialogue" class="btn btn-warning">Cancel</button>
-        </div>
+    <div class="columns is-centered">
+      <form class="form-horizontal">
+          <fieldset>
+              <div class="field">
+                  <label class="label">Menu Item Name</label>
+                  <div class="control">
+                      <input v-model="itemName" type="text" class="input is-rounded">
+                  </div>
+              </div>
+              <div class="field">
+                  <label class="label">Description</label>
+                  <div class="control">                     
+                    <textarea class="textarea is-rounded" v-model = "itemDescription"></textarea>
+                  </div>
+                </div>
+                <label class="label"></label>
+                <div class="field has-addons">
+                  <div class="control">
+                    <a class="button is-static">
+                      Price
+                    </a>
+                  </div>
+                  <div class="control">
+                    <input v-model="itemPrice" class="input is-rounded" type="text">
+                  </div>
+                </div>
+                  <button type="button" class="button is-black" @click="onSubmit">Add to Menu</button>
+                  <button @click="collaspeDialogue" class="button is-light">Cancel</button>
+          </fieldset>
       </form>
     </div>
+  `
+})
+
+Vue.component('headings', {
+  data: function() {
+    return {
+
+    }
+  },
+  template: `
+  <div>
+    <section class="hero is-primary" style="background-color: black;">
+      <div class="hero-body">
+          <div class="container">
+              <h1 class="title is-1" style="text-align: center;">Restaurant Menu Manager</h1>
+          </div>
+      </div>
+    </section>
+  </div>
+  `
+})
+
+Vue.component('menuitems', {
+  data: function() {
+    return {
+      items: items,
+      toedit: null
+    }
+  }, 
+  methods: {
+    deleteitem(item) {
+      for(var i=0; i<items.length; i++){
+        if (items[i].name === item.name) {
+          items.splice(i, 1);
+          break;
+        }
+      }
+      firebase.database().ref(item.name).remove()
+    },
+    edititem(item){
+      toedit=item.name
+      this.$emit('change-data', toedit)
+      this.$emit('show-change-dialogue')
+    }
+  },
+  template: `
+  <div>
+    <section class="section">
+      <div class="container">
+          <div class="columns is-multiline is-mobile">
+            <div class="column is-half" v-for="items in items">
+              <div class="card">
+                <div class="card-content">
+                  <div class="media">
+                    <div class="media-content">
+                      <p class="title is-4">{{ items.name }}</p>\
+                      <p class="subtitle is-6">{{ items.price }}</p>
+                    </div>
+                  </div>
+              
+                  <div class="content">
+                    {{ items.description }}
+                    <br>
+                  </div>
+                  <button id="item" @click="deleteitem(items)" class="button is-danger">Delete</button>
+                  <button id="item" @click="edititem(items)" class="button is-light">Edit</button>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+    </section>
+  </div>
   `
 })
 
@@ -85,37 +175,35 @@ var app = new Vue({
       showChangeDialogue: false
     },
     methods: {
-      deleteitem(item) {
-        for(var i=0; i<items.length; i++){
-          if (items[i].name === item.name) {
-            items.splice(i, 1);
-            break;
-          }
-        }
-        firebase.database().ref(item.name).remove()
-      },
       edititem(item) {
-        this.toedit = item.name
+        this.toedit = item
       },
       changeItem() {
-        for(var i=0; i<items.length; i++) {
-          if (items[i].name === this.toedit) {
-            items[i].name = this.new_name
-            items[i].description = this.new_description
-            items[i].price = this.new_price
+        if(this.new_name && this.new_description && this.new_price && !isNaN(this.new_price)) {
+          for(var i=0; i<items.length; i++) {
+            if (items[i].name === this.toedit) {
+              items[i].name = this.new_name
+              items[i].description = this.new_description
+              items[i].price = this.new_price
+            }
           }
+          firebase.database().ref(this.new_name).set({
+            name: this.new_name,
+            description: this.new_description,
+            price : this.new_price
+          });
+          if(this.new_name != this.toedit){
+            firebase.database().ref(this.toedit).remove()
+          }
+          new_name = ""
+          new_description = ""
+          new_price = ""
+        } else {
+          alert(`Please fill in the dialogue correctly!`)
         }
-        firebase.database().ref(this.new_name).set({
-          name: this.new_name,
-          description: this.new_description,
-          price : this.new_price
-        });
-        firebase.database().ref(this.toedit).remove()
       },
       hideDialogue() {
         this.showItemDialogue = false
       }
     }
   })
-
-
